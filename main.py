0883378f9886c18c345dd0d86fca98e76cc1b166
mkaastez-cmd <mkaastez@gmail.com>
2025-10-31 17:39:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -104,3 +104,12 @@ async def main():
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
+    from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
